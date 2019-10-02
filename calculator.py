970f7d1796cc157ef4fd2cbf9537b0b34d080f21
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-02 16:53:02 -0700

    Made a lookup table and fixed so multiple expressions can be evaluates.

--- calculator.py
+++ calculator.py
@@ -8,33 +8,27 @@ from arithmetic import *
 
 def do_setup():
 
-    input_element = input_token() #[pow. 2. 3]
-    num1 = input_element[1]
-    num2 = input_element[2]
-    num3 = input_element[3]
-    user_input = input_element[0]
+    user_input = ""
+    
+    lookup = {'+': add,
+              '-': subtract,
+              '*': multiply,
+              '/': divide,
+              'square': square,
+              'cube': cube,
+              'mod': mod,
+              'pow': power,
+              'x+' : add_mult,
+              'cubes+' : add_cubes}
 
     while user_input != "q":
-        if user_input == "+":
-            return (add(num1, num2))
-        elif user_input == "-":
-            return (substract(num1, num2))
-        elif user_input == "*":
-            return (multiply(num1. num2))
-        elif user_input == "/":
-            return (divide(num1, num2))
-        elif user_input == "square":
-            return (square(num1))
-        elif user_input == "cube":
-            return (cube(num1))
-        elif user_input == "power":
-            return (power(num1, num2))
-        elif user_input == "mod":
-            return (mod(num1, num2))
-        elif user_input == "x+":
-            return (add_mult(num1, num2, num3))
-        elif user_input == "cubes+":
-            return (add_cubes(num1, num2))
+        input_element = input_token() #[pow. 2. 3]
+        num1 = int(input_element[1])
+        num2 = int(input_element[2])
+        user_input = input_element[0]
+        print(lookup[user_input](num1,num2))
+
+
 
 
 def input_token():
