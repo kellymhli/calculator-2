abc942d3a65173b72443e6add11aa259d0815f56
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-02 19:31:20 -0700

    Added a main function and changed numbers from ints to floats in calculator.py. Added comments.

--- calculator.py
+++ calculator.py
@@ -6,39 +6,41 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-def do_setup():
+def do_setup(tokens):
+    """Prompt the user"""
 
-    user_input = ""
-    
-    lookup = {'+': add,
-              '-': subtract,
-              '*': multiply,
-              '/': divide,
-              'square': square,
-              'cube': cube,
-              'mod': mod,
-              'pow': power,
-              'x+' : add_mult,
-              'cubes+' : add_cubes}
+    # Create a dictionary for the user input command that correspond to the functions in the arithmetic module.
+    lookup = {'+': add, '-': subtract, '*': multiply, '/': divide,
+              'square': square, 'cube': cube, 'mod': mod, 'pow': power,
+              'x+' : add_mult, 'cubes+' : add_cubes}
 
-    while user_input != "q":
-        input_element = input_token() #[pow. 2. 3]
-        num1 = int(input_element[1])
-        num2 = int(input_element[2])
-        user_input = input_element[0]
-        print(lookup[user_input](num1,num2))
+    command = tokens[0]
+    num1 = float(tokens[1])
+    num2 = float(tokens[2])
+
+    # Look up the command that in the dictionary and run the arithetic function using the provided numbers. 
+    if command in lookup:
+        print(lookup[command](num1,num2))
+    # If no such arithmetic function exists, print an error and exit our of the function.
+    else:
+        print("That function is not one of your options.")
+        return
 
 
+def split_token(user_input):
+    """Split string by white space and return the resulting list."""
 
-
-def input_token():
-    """Take in user input and split elements into a list."""
-
-    user_input = input("")
     tokens = user_input.split(" ")
     return(tokens)
 
-do_setup()
+
+def main():
+    user_input = ""
+
+    while user_input != "q":
+        user_input = input("> ")
+        tokens = split_token(user_input)
+        do_setup(tokens)
 
 
-# Your code goes here
+main()
